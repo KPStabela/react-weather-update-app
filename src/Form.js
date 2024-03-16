28625a475ab112b0b5cde83cdc32ee3de63674af
KP-Stabela <159870825+KP-Stabela@users.noreply.github.com>
2024-03-17 00:06:45 +0200

    injected some new css and jsx and added a svg illustration drawing

--- src/Form.js
+++ src/Form.js
@@ -33,10 +33,20 @@ export default function Form() {
 
     let form = (
         <div className="Form">
+            <h2><span>🌀</span> Current Update</h2>
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Enter city..." className="input-field" onChange={updateCity}/>
                 <input type="submit" className="input-btn"/>
             </form>
+            <div>
+                <h1>Klersdorp</h1>
+                <p>Saturday 23:34, clear sky <br /> Humidity: <strong>30%, Wind: <strong>0.74km/h</strong></strong> </p>
+            </div>
+            <div>
+               <span className="weather-emoji">🌖</span> 
+               <span className="weather-temp">20</span>
+               <span className="weather-degree">°C</span>
+            </div>
         </div>
     );
 
